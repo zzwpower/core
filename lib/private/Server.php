8913cfe4a68c941ce8c703ae286879e3bc3fd8d6
Jörn Friedrich Dreyer <jfd@butonic.de>
2017-09-05 09:51:00 +0200

    IRouter is deprecated, deprecate it in the server container

--- lib/private/Server.php
+++ lib/private/Server.php
@@ -1164,6 +1164,8 @@ class Server extends ServerContainer implements IServerContainer {
 	 * Returns a router for generating and matching urls
 	 *
 	 * @return \OCP\Route\IRouter
+	 * @since 7.0.0
+	 * @deprecated 9.0.0
 	 */
 	public function getRouter() {
 		return $this->query('Router');
