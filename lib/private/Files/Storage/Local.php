aa4a908153dd8ff339ee859642a129b7c01763c9
Ilja Neumann <ineumann@owncloud.com>
2017-09-13 17:11:26 +0200

    Replacing specific symlink check with more generic is_readable

--- lib/private/Files/Storage/Local.php
+++ lib/private/Files/Storage/Local.php
@@ -373,9 +373,8 @@ class Local extends \OC\Files\Storage\Common {
 			$realPath = $realPath . '/';
 		}
 
-		// Is broken symlink?
-		if (is_link($fullPath) && !file_exists($fullPath)) {
-			throw new ForbiddenException("$fullPath is a broken/dead symlink", false);
+		if (!is_readable($fullPath)) {
+			throw new ForbiddenException("Can`t read $fullPath", false);
 		}
 
 		if (substr($realPath, 0, $this->dataDirLength) === $this->realDataDir) {
